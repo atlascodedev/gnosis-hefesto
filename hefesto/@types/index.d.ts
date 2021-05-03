71fa8c4c5026a810403914bc241389cca3f0d096
oparin10 <alex.xande10@gmail.com>
2021-05-03 18:32:03 -0300

    [Refactor] Moved hefesto view types to type module

--- hefesto/@types/index.d.ts
+++ hefesto/@types/index.d.ts
@@ -29,3 +29,131 @@ export type BrandingConfig = {
   companyWebsite: string | null;
   logoUrl: string | null;
 };
+
+export type DashboardItemCategory = "creation" | "visualization";
+
+export interface DashboardItemRoot {
+  itemID: string;
+  routerPath: string;
+  sidebarLabel: string;
+  sidebarIcon: IconTypes;
+  itemCategory: DashboardItemCategory;
+}
+
+export interface CheckboxField extends RadioField {}
+
+export interface RadioField {
+  value: string;
+  label: string;
+}
+
+export interface FieldGroup {
+  id: string;
+  label: string;
+}
+
+export interface ListFieldOptions {
+  label: string;
+  fieldLabel: string;
+  min?: number;
+  max?: number;
+}
+
+export interface Attribute {
+  label: string;
+  name: string;
+  attributeValues: Array<string>;
+}
+
+export interface AttributeCollectionField {
+  label: string;
+  name: string;
+}
+
+export interface Category {
+  uuid: string;
+  root?: boolean;
+  label: string;
+  parent: string | null | undefined;
+  uuid_path: Array<string>;
+  label_path?: Array<string>;
+}
+
+export type DashboardItem = DataCreationItem;
+
+export interface DataCreationItem extends DashboardItemRoot {
+  collectionRef: string;
+  fields: Array<DataCreationField>;
+  attributesFields?: Array<AttributeCollectionField>;
+  hasCategories?: boolean | null;
+  hasAttributes?: boolean | null;
+  fieldGroups?: FieldGroup[];
+  showID?: boolean;
+}
+
+export interface DataCreationField {
+  fieldType: FormFieldTypes;
+  label: string;
+  initialValue?: string | Array<string>;
+  selectOptions?: Array<string>;
+  radioOptions?: Array<RadioField>;
+  checkboxOptions?: Array<CheckboxField>;
+  listOptions?: ListFieldOptions;
+  hidden?: boolean;
+  private?: boolean;
+  required?: boolean;
+  currencyPrefix?: string;
+  slug?: boolean;
+  groupID?: string;
+  name: string;
+}
+
+export type FormFieldTypes =
+  | "string"
+  | "select"
+  | "text"
+  | "image"
+  | "money"
+  | "phone"
+  | "markdown"
+  | "boolean"
+  | "radio"
+  | "checkbox"
+  | "date"
+  | "time"
+  | "list"
+  | "installment"
+  | "slug";
+
+export type IconTypes =
+  | "AttachMoney"
+  | "AccountBalance"
+  | "AcUnit"
+  | "AccessAlarm"
+  | "Accessibility"
+  | "Accessible"
+  | "AccountBox"
+  | "Add"
+  | "AddAPhoto"
+  | "AddAlert"
+  | "AddCircle"
+  | "AddComment"
+  | "AddPhotoAlternate"
+  | "AddShoppingCart"
+  | "Apps"
+  | "PhotoLibrary"
+  | "MoreHoriz"
+  | "Close"
+  | "Settings"
+  | "FileCopy"
+  | "Delete"
+  | "DeleteForever"
+  | "Category"
+  | "Palette"
+  | "Panorama"
+  | "Build"
+  | "SubdirectoryArrowRight"
+  | "ArrowBackRounded"
+  | "LibraryBooks"
+  | "CardTravel"
+  | "Group";
--- hefesto/view/src/config/collections.config.ts
+++ hefesto/view/src/config/collections.config.ts
@@ -1,82 +1,16 @@
-import { IconTypes, FormFieldTypes } from "../dictionaries/types";
-
-export interface FieldGroup {
-  id: string;
-  label: string;
-}
-
-export type DashboardItemCategory = "creation" | "visualization";
-
-export interface DashboardItemRoot {
-  itemID: string;
-  routerPath: string;
-  sidebarLabel: string;
-  sidebarIcon: IconTypes;
-  itemCategory: DashboardItemCategory;
-}
-
-export interface RadioField {
-  value: string;
-  label: string;
-}
-
-export interface CheckboxField extends RadioField {}
-
-export interface DataCreationField {
-  fieldType: FormFieldTypes;
-  label: string;
-  initialValue?: string | Array<string>;
-  selectOptions?: Array<string>;
-  radioOptions?: Array<RadioField>;
-  checkboxOptions?: Array<CheckboxField>;
-  listOptions?: ListFieldOptions;
-  hidden?: boolean;
-  private?: boolean;
-  required?: boolean;
-  currencyPrefix?: string;
-  slug?: boolean;
-  groupID?: string;
-  name: string;
-}
-
-export interface ListFieldOptions {
-  label: string;
-  fieldLabel: string;
-  min?: number;
-  max?: number;
-}
-
-export interface DataCreationItem extends DashboardItemRoot {
-  collectionRef: string;
-  fields: Array<DataCreationField>;
-  attributesFields?: Array<AttributeCollectionField>;
-  hasCategories?: boolean | null;
-  hasAttributes?: boolean | null;
-  fieldGroups?: FieldGroup[];
-  showID?: boolean;
-}
-
-export interface Attribute {
-  label: string;
-  name: string;
-  attributeValues: Array<string>;
-}
-
-export interface AttributeCollectionField {
-  label: string;
-  name: string;
-}
-
-export interface Category {
-  uuid: string;
-  root?: boolean;
-  label: string;
-  parent: string | null | undefined;
-  uuid_path: Array<string>;
-  label_path?: Array<string>;
-}
-
-export type DashboardItem = DataCreationItem;
+import {
+  IconTypes,
+  FormFieldTypes,
+  FieldGroup,
+  RadioField,
+  CheckboxField,
+  DashboardItemRoot,
+  DashboardItemCategory,
+  DataCreationField,
+  ListFieldOptions,
+  DataCreationItem,
+  DashboardItem,
+} from "@hefesto/types";
 
 const cardCollection: DashboardItem = {
   showID: true,
